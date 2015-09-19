6738cf9ed9ebea2ede6acbdb13d1f889b79fc015
liyakai <liyakai@mail.ustc.edu.cn>
2015-09-19 22:23:08 +0800

    unfinished

---
+++ blockingQueue/src/cn/liyakai/blockingQueue/BlockingQueueTest.java
@@ -0,0 +1,28 @@
+package cn.liyakai.blockingQueue;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+public class BlockingQueueTest {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter base directory (e.g. /usr/local/jdk1.6.0/src):");
+		String directory = in.nextLine();
+		System.out.print("Enter keyword (e.g. volatile):");
+		String keyword = in.nextLine();
+		final int File_QUEUE_SIZE = 10;
+		final int SEARCH_THREADS = 100;
+		
+		BlockingQueue<File> queue = new ArrayBlockingQueue<>(FILE_QUEUE_SIZE);
+		
+		FileEnumerationTask enumerator = new FileEnumerationTask(queue, new File(directory));
+		new Thread(enumerator).start();
+		for (int i = 1; i <= SEARCH_THREADS; i++)
+			new Thread(new SearchTask(queue, keyword)).start();
+	}
+}
+
+/*
+ * This task enumerates all files in a directory and its sundirectories.
+ * */
